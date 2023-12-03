5dec6f19b2aa3e757067cc78d421df14c6252d9f
Joshua Lehman <joshlehman.dev@gmail.com>
2023-12-02 21:17:14 -0500

    updates

--- medusa-storefront/src/app/(main)/page.tsx
+++ medusa-storefront/src/app/(main)/page.tsx
@@ -1,14 +1,14 @@
 import { getCollectionsList } from "@lib/data"
 import FeaturedProducts from "@modules/home/components/featured-products"
 import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
-import { Metadata } from "next"
+// import { Metadata } from "next"
 import { Suspense } from "react"
 
-export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
-  description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
-}
+// export const metadata: Metadata = {
+//   title: "Medusa Next.js Starter Template",
+//   description:
+//     "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+// }
 
 export default async function Home() {
   const { collections, count } = await getCollectionsList(0, 3)
